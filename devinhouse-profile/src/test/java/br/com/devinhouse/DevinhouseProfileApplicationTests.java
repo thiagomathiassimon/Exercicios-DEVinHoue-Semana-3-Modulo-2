038c88a7c8a9d1bed511e79438ba0cff62bddfcb
Thiago Mathias Simon <thiago_m_simon@estudante.sc.senai.br>
2021-06-27 17:58:52 -0300

    Exercicio-4

--- devinhouse-profile/src/test/java/br/com/devinhouse/DevinhouseProfileApplicationTests.java
+++ devinhouse-profile/src/test/java/br/com/devinhouse/DevinhouseProfileApplicationTests.java
@@ -1,10 +1,10 @@
-package exercicios.profile.devinhouse.demo;
+package br.com.devinhouse;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class DevinhouseProfileApplicationTests {
 
 	@Test
 	void contextLoads() {
